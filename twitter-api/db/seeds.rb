a8a96da050416f22efaa6863a7550b476cbcb4d0
Crystal Free <cdmackeyfree@gmail.com>
2018-06-01 15:49:31 -0400

    seed fixed

--- twitter-api/db/seeds.rb
+++ twitter-api/db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-users = User.create([{username: 'Boots', user_id: 1}{username: 'Marmie', user_id: 2}{username: 'Bananas', user_id: 3}])
-posts = Post.create([{body: "I like hiking", user_id: 1}{body:'Rootin Tootin', user_id: 2}{body:'I like to think I am apeeling', user_id:3}])
+users = User.create([{username: 'Boots'},{username: 'Marmie'},{username: 'Bananas'},{username: 'Pants'},{username: 'Veronica'},{username: 'Joe'} ])
+posts = Post.create([{body: "I like hiking", user_id: 1},{body:'Rootin Tootin', user_id: 2},{body:'I like to think I am apeeling', user_id:3}])
 
