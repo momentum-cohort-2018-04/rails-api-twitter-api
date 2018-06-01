c7dd05acdfd4ff8f7a69374ec94d87f536c48e30
Jacobfh <jfh708@gmail.com>
2018-06-01 12:42:31 -0400

    adds seed for user

--- twitter-api/app/controllers/application_controller.rb
+++ twitter-api/app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::API
+    include ActionController::HttpAuthentication::Token::ControllerMethods
+    before_action :verify_authentication, except :index
+  
+    def verify_authentication
+      user = authenticate_with_http_token do |token, options|
+        User.find_by(api_token: token)
+      end
+  
+      unless user
+        render json: { error: "You do not have permission to access these resources" }, status: :unauthorized
+      end
+    end
     helper_method :current_user
 
     def current_user    
