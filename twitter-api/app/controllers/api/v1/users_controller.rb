b7ff8d7c5de9a952af6f8948b2e1a9257d6b1c4e
Crystal Free <cdmackeyfree@gmail.com>
2018-06-02 10:15:03 -0400

    adds show for user

--- twitter-api/app/controllers/api/v1/users_controller.rb
+++ twitter-api/app/controllers/api/v1/users_controller.rb
@@ -6,6 +6,9 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    @posts = Post.find_by(@user.id)
+    render json: @user.posts
   end
 
   def create
