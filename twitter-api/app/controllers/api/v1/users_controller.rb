6eae3fe44a8426b0a734af3fcd94af69ee2bbd9d
Crystal Free <cdmackeyfree@gmail.com>
2018-06-02 10:40:48 -0400

    MVP working

--- twitter-api/app/controllers/api/v1/users_controller.rb
+++ twitter-api/app/controllers/api/v1/users_controller.rb
@@ -7,8 +7,7 @@ class Api::V1::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = Post.find_by(@user.id)
-    render json: @user.posts
+    render json: @user
   end
 
   def create
